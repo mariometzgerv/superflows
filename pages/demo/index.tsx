7744a676c07093de3bb5a8e502a4ca2183011f95
Mario Antonio Metzger Valdivia <mariometzgerv@gmail.com>
2024-08-21 01:08:54 -0400

    Integración de SuperFlows React UI para Demo

---
+++ pages/demo/index.tsx
@@ -0,0 +1,25 @@
+import { useProfile } from "../../components/contextManagers/profile";
+import { SuperflowsButton } from "@superflows/chat-ui-react";
+import { useRouter } from "next/router";
+import { LoadingPage } from "../../components/loadingspinner";
+import React from "react";
+
+export default function Demo() {
+  const { profile } = useProfile();
+  const router = useRouter();
+
+  if (profile === undefined) return <LoadingPage />;
+  else if (!profile?.org_id) {
+    router.push("/onboarding");
+    return <LoadingPage />;
+  }
+
+  return (
+    <div>
+      <SuperflowsButton
+        superflowsUrl={process.env.NEXT_PUBLIC_SUPERFLOWS_URL}
+        superflowsApiKey={process.env.NEXT_PUBLIC_SUPERFLOWS_API_KEY as string}
+      ></SuperflowsButton>
+    </div>
+  );
+}
